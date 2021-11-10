3e25d99f23bb68f5ac075f63c460abe7905fc0ba
zunayed-arp <zunayed.arp@gmail.com>
2021-11-10 15:47:23 +0600

    remove local date string

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ async function run() {
 
 		app.get('/appointments', verifyToken, async (req, res) => {
 			const email = req.query.email;
-			const date = new Date(req.query.date).toLocaleDateString();
+			// const date = new Date(req.query.date).toLocaleDateString();
+			const date =req.query.date;
 			// console.log(date)
 			const query = { email: email, date: date };
 			const cursor = appointmentsCollection.find(query);
